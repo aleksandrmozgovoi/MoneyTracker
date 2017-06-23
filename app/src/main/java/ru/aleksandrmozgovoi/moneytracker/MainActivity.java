ccaa483964c2a70474278b890cd186c40462a337
NightScream <night>
2017-06-23 10:23:50 +0300

    add screen layout New

--- app/src/main/java/ru/aleksandrmozgovoi/moneytracker/MainActivity.java
+++ app/src/main/java/ru/aleksandrmozgovoi/moneytracker/MainActivity.java
@@ -2,6 +2,11 @@ package ru.aleksandrmozgovoi.moneytracker;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +14,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final TextView add = (TextView) findViewById(R.id.add);
+        final EditText name = (EditText) findViewById(R.id.name);
+        final EditText money = (EditText) findViewById(R.id.money);
+        TextWatcher textWatcher = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                /**Проверка на пустые поля*/
+                add.setEnabled(!TextUtils.isEmpty(name.getText().toString()) && !TextUtils.isEmpty(money.getText().toString()));
+            }
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        };
+        name.addTextChangedListener(textWatcher);
+        money.addTextChangedListener(textWatcher);
     }
 
     @Override
